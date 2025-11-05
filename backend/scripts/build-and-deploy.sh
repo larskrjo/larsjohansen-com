909c5ed582691f25b9e102894c9d1cf8b7231645
Lars Johansen <larskrjo@gmail.com>
2025-11-05 10:44:39 -0800

    more stuff

--- backend/scripts/build-and-deploy.sh
+++ backend/scripts/build-and-deploy.sh
@@ -1,6 +1,6 @@
 pushd /home/ec2-user/larsjohansen-com/backend
 docker build -t larsjohansen-backend:latest .
-docker compose down
+docker compose -p larsjohansen-backend down
 docker compose -p larsjohansen-backend up -d
 docker image prune -a -f
 popd
